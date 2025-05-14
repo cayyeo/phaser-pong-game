c0e8ca143fd7d0d3004fb2df0203cd50267929a8
cayyeo <cayden_yeo_li_sen@students.edu.sg>
2025-05-14 08:12:07 +0000

    basic adds

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -6,12 +6,22 @@ const HEIGHT = 768;
 export class Game extends Scene {
     constructor() {
         super('Game');
+        this.ball = null;
+        this.leftPaddle = null;
+        this.rightPddle = null;
     }
 
     preload() {
+        this.load.image('background','assets/ball.png');
+        this.load.image('ball','assets/ball.png');
+        this.load.image('paddle','assets/paddle.png');
     }
 
     create() {
+        this.image(WIDTH/2, HEIGHT/2, 'background').setScale(0.8, 0.8);
+        this.ball = this.add.image(WIDTH/2, HEIGHT/2, 'ball').setScale(0.05, 0.05);
+        this.leftPaddle = this.add.image(50, 384, 'paddle');
+        this.rightPaddle = this.add.image(974, 384, 'paddle');
     }
 
     update() {
