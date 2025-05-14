e459ee280a2f96db92b19e86e2758359f4d431ce
cayyeo <cayden_yeo_li_sen@students.edu.sg>
2025-05-14 09:22:37 +0000

    extra changes

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -9,19 +9,24 @@ export class Game extends Scene {
         this.ball = null;
         this.leftPaddle = null;
         this.rightPaddle = null;
+        this.ballinMotion = false;
+
     }
 
     preload() {
-        this.load.image('background','assets/background.png');
-        this.load.image('ball','assets/ball.png');
-        this.load.image('paddle','assets/paddle.png');
+        this.load.image('background', 'assets/background.png');
+        this.load.image('ball', 'assets/ball.png');
+        this.load.image('paddle', 'assets/paddle.png');
     }
 
     create() {
-        this.image(WIDTH/2, HEIGHT/2, 'background').setScale(0.8, 0.8);
-        this.ball = this.add.image(WIDTH/2, HEIGHT/2, 'ball').setScale(0.05, 0.05);
+        this.add.image(WIDTH/2, HEIGHT/2, 'background').setScale(0.8, 0.8);
         this.leftPaddle = this.add.image(50, 384, 'paddle');
         this.rightPaddle = this.add.image(974, 384, 'paddle');
+        this.ball = this.physics.add.image(WIDTH/2, HEIGHT/2, 'ball').setScale(0.05, 0.05).refreshBody();
+        this.ball.setCollideWorldBounds(true);
+        this.ball.setBounce(1, 1);
+        this.ball.setVelocity(200, 200);
     }
 
     update() {
